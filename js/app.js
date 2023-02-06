df2fe55d280407b248ceff6238489f631a357033
Isk-On <119677180+Isk-On@users.noreply.github.com>
2023-02-06 16:36:37 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// 1
+// for (let i = 0; i < 100; i++) {
+//     if(i%2 === 0){
+//         console.log(i)
+//     }
+// }
+
+// 2
+// for (let i = 0; i < 100; i++) {
+//     if(i%3 === 0 && i%5 === 0){
+//         console.log(i)
+//     }
+// }
+
+// 3
+
+// while (true) {
+//   const number = +prompt("Вdежите число");
+//   if (number <= 100) {
+//     alert("Попробуйте еще раз");
+//   } else {
+//     break;
+//   }
+// }
+
+// 4
+
+// const number = +prompt("Введите число");
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${number} * ${i} = ${number * i}`);
+// }
